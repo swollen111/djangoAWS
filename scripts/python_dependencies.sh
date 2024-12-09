c5667fd216e22cbf2a74799c5e04a4945db02b27
swollen111 <beeyt22@gmail.com>
2024-12-09 18:08:09 +0100

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -24,5 +24,6 @@ source "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/venv/bin/activate"
 echo "Installing Python dependencies..."
 pip install -r "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/requirements.txt"
 pip install django-crispy-forms
+python3 -m pip install Pillow
 
 echo "Dependencies installed successfully."
