2bb935a7aee1e528901f23fe596112d01aa62362
swollen111 <beeyt22@gmail.com>
2024-12-10 21:28:50 +0100

    Update apps.py

--- carro/apps.py
+++ carro/apps.py
@@ -3,3 +3,19 @@ from django.apps import AppConfig
 
 class CarroConfig(AppConfig):
     name = 'carro'
+
+# Este archivo define la configuración de la aplicación Django llamada "carro".
+# El archivo es generado automáticamente al crear una nueva aplicación con Django.
+
+# 1. `from django.apps import AppConfig`:
+#    - Importa la clase `AppConfig` de Django, que es la clase base utilizada para definir la configuración de una aplicación Django.
+
+# 2. `class CarroConfig(AppConfig):`:
+#    - Define la clase `CarroConfig` que hereda de `AppConfig`.
+#    - Esta clase contiene la configuración específica para la aplicación "carro".
+
+# 3. `name = 'carro'`:
+#    - Especifica el nombre de la aplicación como `'carro'`.
+#    - Este nombre se utiliza para que Django reconozca la aplicación dentro del proyecto y la registre correctamente.
+
+# En resumen, este archivo configura la aplicación "carro" dentro de tu proyecto Django, permitiendo que Django gestione esta aplicación correctamente.
